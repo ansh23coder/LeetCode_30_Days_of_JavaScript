435509c9fb217679eb494139e6c593774f47183c
Anupam Sharma <ansh151923@gmail.com>
2025-05-22 18:54:16 +0530

    adding leetcode88

---
+++ OtherJavaScriptQuestions/88_MergeSortedArray.js
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/merge-sorted-array/
+
+var merge = function(nums1, m, nums2, n) {
+    for (let i = m, j = 0; j < n; i++, j++) {
+        nums1[i] = nums2[j];
+    }
+    nums1.sort((a, b) => a - b);
+};
