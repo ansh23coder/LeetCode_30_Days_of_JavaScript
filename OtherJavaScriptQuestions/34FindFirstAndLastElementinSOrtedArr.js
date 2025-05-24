97035340d5612e554162f8b2d31c7731b013f50a
Anupam Sharma <ansh151923@gmail.com>
2025-05-24 10:02:54 +0530

    adding 34 leetcode

---
+++ OtherJavaScriptQuestions/34FindFirstAndLastElementinSOrtedArr.js
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    let startingPosition = -1;
+    let endingPosition = -1;
+    let n = nums.length;
+
+    // Find first occurrence -
+    for (let i = 0; i < n; i++) {
+        if (nums[i] === target) {
+            startingPosition = i;
+            break;
+        }
+    }
+    // Find last occurrence - 
+    for (let i = n - 1; i >= 0; i--) {
+        if (nums[i] === target) {
+            endingPosition = i;
+            break;
+        }
+    }
+    return [startingPosition, endingPosition];
+};
