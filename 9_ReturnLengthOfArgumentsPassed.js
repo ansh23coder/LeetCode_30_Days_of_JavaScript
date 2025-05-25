cd06ec95f6079589fe1ce65b69b7bf71ac6776f4
Anupam Sharma <ansh151923@gmail.com>
2025-05-25 10:13:05 +0530

    add 2703 leetcode

---
+++ 9_ReturnLengthOfArgumentsPassed.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/return-length-of-arguments-passed/
+ 
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    let count = 0;
+    for(let i = 0; i < args.length; i++){
+        count++;
+    }
+    return count;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
