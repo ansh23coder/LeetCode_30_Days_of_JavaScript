7024b8c027fbb6aeb7f4168a42c7a067fb2fe698
Anupam Sharma <ansh151923@gmail.com>
2025-05-28 21:38:50 +0530

    updating

--- 12_AddTwoPromises.js
+++ 12_AddTwoPromises.js
@@ -1,6 +1,5 @@
 // https://leetcode.com/problems/add-two-promises/
 
-
 /**
  * @param {Promise} promise1
  * @param {Promise} promise2
@@ -9,6 +8,7 @@
 var addTwoPromises = async function(promise1, promise2) {
   // Wait for both promises to resolve and retrieve their values
   const [value1, value2] = await Promise.all([promise1, promise2]);
+  
   // Return a new promise that resolves with the sum of the values
   return value1 + value2;
 };
@@ -17,4 +17,3 @@ var addTwoPromises = async function(promise1, promise2) {
  * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
  *   .then(console.log); // 4
  */
- 
