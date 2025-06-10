e83902608ca9a4aa1af73c89e08430fcdfef6840
Anupam Sharma <ansh151923@gmail.com>
2025-06-10 08:48:56 +0530

    adding 2722 leetcode

---
+++ 25_JoinTwoArraysById.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/join-two-arrays-by-id/
+
+
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+var join = function(arr1, arr2) {
+    let result = {};
+
+    arr1.forEach(obj => {
+        result[obj.id] = { ...result[obj.id], ...obj };
+    });
+
+    arr2.forEach(obj => {
+        result[obj.id] = { ...result[obj.id], ...obj };
+    });
+
+    return Object.values(result).sort((a, b) => a.id - b.id);
+};
