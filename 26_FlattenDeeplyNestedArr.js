64a462dd1e71efc11e4ffe16d3505d2d35d8c91d
Anupam Sharma <ansh151923@gmail.com>
2025-06-12 11:24:49 +0530

    update 2625

--- 26_FlattenDeeplyNestedArr.js
+++ 26_FlattenDeeplyNestedArr.js
@@ -5,19 +5,18 @@
  * @param {number} depth
  * @return {any[]}
  */
-var flat = function(arr, depth) {
-  const stack = [...arr.map(item => [item, depth])];
-  const result = [];
+var flat = function (arr, depth) {
+    const stack = [...arr.map(item => [item, depth])];
+    const result = [];
 
-  while (stack.length > 0) {
-    const [item, depth] = stack.pop();
+    while (stack.length > 0) {
+        const [item, depth] = stack.pop();
 
-    if (Array.isArray(item) && depth > 0) {
-      stack.push(...item.map(subItem => [subItem, depth - 1]));
-    } else {
-      result.push(item);
+        if (Array.isArray(item) && depth > 0) {
+            stack.push(...item.map(subItem => [subItem, depth - 1]));
+        } else {
+            result.push(item);
+        }
     }
-  }
-
-  return result.reverse();
+    return result.reverse();
 };
