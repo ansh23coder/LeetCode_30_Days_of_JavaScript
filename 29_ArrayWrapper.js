4fcf7e0b9eb94eb6aadb7b754e628508632ec986
Anupam Sharma <ansh151923@gmail.com>
2025-06-15 13:50:56 +0530

    adding lt 2695

---
+++ 29_ArrayWrapper.js
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/array-wrapper/
+
+class ArrayWrapper {
+    constructor(nums) {
+        this.nums = nums;
+    }
+
+    valueOf() {
+        let sum = 0;
+        for (let i = 0; i < this.nums.length; i++) {
+            sum += this.nums[i];
+        }
+        return sum;
+    }
+
+    toString() {
+        let str = '[';
+        for (let i = 0; i < this.nums.length; i++) {
+            str += this.nums[i];
+            if (i !== this.nums.length - 1) {
+                str += ',';
+            }
+        }
+        str += ']';
+        return str;
+    }
+}
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
