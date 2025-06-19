6511c863b656f8140c17566b471b8d07f7e67ade
Anupam Sharma <ansh151923@gmail.com>
2025-06-19 08:49:38 +0530

    update 34 leetcode

---
+++ OtherJavaScriptQuestions/2294_PartitionArrSuchThatMaxiDifferIsK.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/partition-array-such-that-maximum-difference-is-k/
+
+
+function partitionArray(nums, k) {
+    nums.sort((a, b) => a - b);
+    let ans = 1;
+    let min = nums[0];
+    
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] - min > k) {
+            ans++;
+            min = nums[i];
+        }
+    }
+    
+    return ans;
+}
