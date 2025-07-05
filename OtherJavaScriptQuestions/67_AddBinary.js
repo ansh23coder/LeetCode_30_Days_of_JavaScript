42d7a836f0bf6ddbc67175864c1c8b061a08c78c
Anupam Sharma <ansh151923@gmail.com>
2025-07-05 15:20:42 +0530

    add leetcode67

--- OtherJavaScriptQuestions/67_AddBinary.js
+++ OtherJavaScriptQuestions/67_AddBinary.js
@@ -1,3 +1,24 @@
-// 
+// https://leetcode.com/problems/add-binary/
 
 // js code:
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+    let ans = '';
+    let carry = 0;
+    let i = a.length - 1;
+    let j = b.length - 1;
+
+    while (i >= 0 || j >= 0 || carry) {
+        if (i >= 0) carry += parseInt(a[i--]);
+        if (j >= 0) carry += parseInt(b[j--]);
+
+        ans = (carry % 2) + ans;
+        carry = Math.floor(carry / 2);
+    }
+
+    return ans;
+};
