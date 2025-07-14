ba15bf024da8f251efec07eea23839349e5028c0
Anupam Sharma <ansh151923@gmail.com>
2025-07-14 11:02:08 +0530

    adding 1290

---
+++ OtherJavaScriptQuestions/1290_CnvrtBinaryNumLinkedListIntoInteger.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var getDecimalValue = function(head) {
+    let result = 0;
+    while (head !== null) {
+        result = (result << 1) | head.val;
+        head = head.next;
+    }
+    return result;
+};
