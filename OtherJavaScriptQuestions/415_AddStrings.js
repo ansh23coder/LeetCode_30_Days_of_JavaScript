93a0931bdf1beb7dbc2504139e951e6e30d0b839
Anupam Sharma <ansh151923@gmail.com>
2025-07-29 22:47:08 +0530

    adding

---
+++ OtherJavaScriptQuestions/415_AddStrings.js
@@ -0,0 +1,30 @@
+//  https://leetcode.com/problems/add-strings/
+
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+    let i = num1.length - 1;
+    let j = num2.length - 1;
+    let carry = 0;
+    let result = '';
+
+    while (i >= 0 || j >= 0 || carry > 0) {
+        const digit1 = i >= 0 ? parseInt(num1[i]) : 0;
+        const digit2 = j >= 0 ? parseInt(num2[j]) : 0;
+
+        const sum = digit1 + digit2 + carry;
+        carry = Math.floor(sum / 10);
+        result += (sum % 10).toString();
+
+        i--;
+        j--;
+    }
+
+    return result.split('').reverse().join('');
+};
+
+// Example usage:
+console.log(addStrings("123", "987"));  // Output: "1110"
