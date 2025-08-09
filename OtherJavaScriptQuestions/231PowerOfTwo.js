c231635e857054117a64caf4dc4da4d6c5c125ea
Anupam Sharma <ansh151923@gmail.com>
2025-08-09 22:17:24 +0530

    added

---
+++ OtherJavaScriptQuestions/231PowerOfTwo.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/power-of-two/
+
+var isPowerOfTwo = function(n) {
+    for (let i = 0; i <= 30; i++) {
+        let ans = Math.pow(2, i);
+        if (ans === n) {
+            return true;
+        }
+    }
+    return false;
+};
