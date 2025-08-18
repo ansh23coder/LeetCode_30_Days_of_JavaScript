6e66c8d3f29ceaadcbdca8ba12ed8cc484a5d16d
Anupam Sharma <ansh151923@gmail.com>
2025-08-18 19:05:11 +0530

    added lt problem

---
+++ OtherJavaScriptQuestions/1323Maximum69Number.js
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/maximum-69-number/
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximum69Number  = function(num) {
+    let s = num.toString().split('');
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '6') {
+            s[i] = '9';
+            break;
+        }
+    }
+    return parseInt(s.join(''));
+};
